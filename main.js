fc16d561b5faac773286870e82a4b6d7521a35ed
selenacatenamorales <catena.morales.selena@alumnat.copernic.cat>
2021-02-01 20:47:50 +0100

    SCM - creacio divs + objecte JSON

---
+++ main.js
@@ -0,0 +1,24 @@
+window.onload = function (){
+
+let personatges = [
+    {
+        id: 1,
+        nom: "Selena",
+        cognom: "Catena",
+        edat: 21,
+        imatge: "imagenes/Monigote.jpeg"
+    }, 
+
+    {
+        id: 2,
+        nom: "Gerard",
+        cognom: "Martinez",
+        edat: 150,
+        imatge: "imagenes/Monigo.png" 
+    }
+]
+
+}
+
+
+
